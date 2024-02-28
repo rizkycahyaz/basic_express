3c413cadf1c5bbd4e906abd029aa3f8946d27381
rizky cahya z <rizkycahyaz22@gmail.com>
2024-02-28 22:02:59 +0700

    tugas 4

--- routes/kategori.js
+++ routes/kategori.js
@@ -16,4 +16,30 @@ router.get('/', function (req, res, next) {
   })  
 })
 
+router.get ('/create', function (req, res, next){
+  res.render ('kategori/create',{
+      nama_kategori : ''
+  })
+})
+
+router.post ('/store', function (req, res, next){
+  try {
+      let {nama_kategori} = req.body;
+      let data = {
+          nama_kategori
+      }
+      connection.query('insert into produk set ?',data, function(err, result){
+          if (err){
+              req.flash('error', 'gagal menyimpan data!!');
+          }else{
+              req.flash('success', 'berhasil mneyimpan data');
+          }
+          res.redirect('/kategori');
+      })
+  } catch {
+      req.flash ('error','terjadi kesalahan pada fungsi');
+      res.redirect ('/kategori'); 
+  }
+})
+
 module.exports = router;
